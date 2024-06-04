29f5fdce33183c07f1e4a806f30eaa7ae007458f
Chanda2003 <venumadhavanchanda@gmail.com>
2024-06-04 16:46:53 +0530

    Remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,96 +1,10 @@
 
-// import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-// import './App.css';
-// import Home from './Container/Home';
-// import React,{ useEffect, useState } from 'react';
-// import { auth, db } from './Config/Firebase.config';
-// import {  collection, doc, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
-// import { useDispatch } from 'react-redux';
-// import { SET_USER } from './Context/Action/UserAction';
-// import Project from './Container/Newproject/NewProject';
-// import {  SET_PROJECTS } from './Context/Action/ProjectAction';
-
-// function App() {
-// const Navigate=useNavigate()
-// const[isLoding,setIsLoding]=useState(true)
-// const dispatch=useDispatch()
-
-// useEffect(()=>{
-//     const unsubscribe=auth.onAuthStateChanged((userCred)=>{
-//       if(userCred){
-//         console.log(userCred?.providerData[0])
-
-//         setDoc(doc(db,"users",userCred?.uid),userCred?.providerData[0])
-//         .then(()=>{
-//           // dispath to redux Store
-
-//           dispatch(SET_USER(userCred?.providerData[0]))
-//           Navigate("/home/projects" ,{replace:true})
-//         })
-
-//       }
-//       else{
-//           Navigate("/home/auth" ,{replace:true})
-//       }
-
-//       setInterval(()=>{
-//        setIsLoding(false)
-//       },2000)
-
-//     })
-//     // to clen up listener Event
-//     return () =>unsubscribe()
-//   },[])
-
-// useEffect(()=>{
-//   const projectQuery=query(
-//     collection(db,"Projects"),
-//     orderBy("id","desc")
-//   )
-//   const unsubscribe=onSnapshot(projectQuery,(querySnaps)=>{
-//     const projectsList=querySnaps.docs.map((doc)=>doc.data())
-//     dispatch(SET_PROJECTS(projectsList))
-//   })
-//   return () => unsubscribe
-
-// },[])
-
-
-
-//   return (
-//     <>
-//   {
-//     isLoding ?
-//     <div className='w-screen h-screen flex items-center justify-center overflow-hidden text-white'> Loding....... </div>
-//     :
-//     <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
-
-//     <Routes>
-//       <Route path='/home/*' element={<Home/>}></Route>
-//       <Route path='/newProject' element={<Project/>}></Route>
-
-// {/* if the route not matching */}
-
-// <Route path='*' element={<Navigate to={"/home"}/>}></Route>
-      
-//     </Routes>
-  
-//   </div>
-//   }
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './Container/Home';
 import React,{ useEffect, useState } from 'react';
 import { auth, db } from './Config/Firebase.config';
-import {  collection, doc, getDoc, getDocs, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
+import {  collection, doc,  getDocs,  setDoc } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { SET_USER } from './Context/Action/UserAction';
 // import Project from './Container/Newproject/NewProject';
--- src/Config/Firebase.config.js
+++ src/Config/Firebase.config.js
@@ -1,4 +1,4 @@
-import { getApps,getApp,initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
